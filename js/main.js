a62d5ee61b8e1f8c249e386d7d7d8f4533a07f90
Ryan Roman <khluxordx@gmail.com>
2016-09-27 10:40:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,20 +88,20 @@ function checkKeypress(e) {
   var keycode = e.keyCode;
     
     if (keycode == 87) {
-        bar1Y = bar1Y - 3;
-        bar2Y = bar2Y - 3;
+        bar1Y = bar1Y - 7;
+        bar2Y = bar2Y - 7;
         updateCanvas();
     } else if (keycode == 83) {
-        bar1Y = bar1Y + 3;
-        bar2Y = bar2Y + 3;
+        bar1Y = bar1Y + 7;
+        bar2Y = bar2Y + 7;
         updateCanvas();
     }
     
     if (keycode == 38) {
-        ballY = ballY - 3;
+        ballY = ballY - 7;
         updateCanvas();
     } else if (keycode == 40) {
-        ballY = ballY + 3;  
+        ballY = ballY + 7;  
         updateCanvas();
     }
   

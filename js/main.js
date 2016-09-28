bb1b077667b709492624c5ada3b23dd1fe624cb7
Ryan Roman <khluxordx@gmail.com>
2016-09-28 11:14:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ function initCanvas() {
             updateGame();
             updateCanvas();
         }
-        gameInt = setInterval(mainloop, 1000 / 120);
+        gameInt = setInterval(mainloop, 1000 / 60);
     
 }
 
@@ -151,20 +151,20 @@ function checkKeyup(e) {
     
     if (falseKeycode[87]) {
      barUp = false;
-     keycode[87] = false;
+     //keycode[87] = false;
     }
     if (falseKeycode[83]) {
      barDown = false;
-     keycode[83] = false;
+     //keycode[83] = false;
     }
     
     if (falseKeycode[38]) {
       ballUp = false;
-      keycode[38] = false;
+      //keycode[38] = false;
     }
     if (falseKeycode[40]) {
         ballDown = false;
-        keycode[40] = false;
+        //keycode[40] = false;
     }
     
 }

e51707f3c27d4c5f7987be359552fd3925185b61
Ryan Roman <khluxordx@gmail.com>
2016-09-29 13:05:23 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -126,6 +126,16 @@ function updateGame() {
     
     ////////////////////////
     
+    //Change Ball Direction/
+    
+    if (barDir && ballX > bar1X + 50 && ballY <= bar1Y && ballY >= bar1Y + 75) {
+        barDir = false;
+        //Increment bar point by .5
+    } else if (!barDir && ballX < bar2X - 50 && ballY <= bar1Y && ballY >= bar2Y + 75) {
+        barDir = true;
+        //Increment bar point by .5
+    }
+    
 }
 
 function checkMousePos(e) {

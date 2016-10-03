edd31901371b4f16c466b8c15ab4eee29e4c02c5
Ryan Roman <khluxordx@gmail.com>
2016-10-03 13:12:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -84,6 +84,7 @@ function drawImg() { //Draw images onto the screen
   draw.drawImage(bar, bar1X, bar1Y);
   draw.drawImage(bar, bar2X, bar2Y);
   draw.drawImage(playerBall, ballX, ballY);
+  draw.font = "20px Georgia";
   draw.fillStyle = "#ffffff";
   draw.fillText("Bar: " + barScore, 10, 50, 200);
   draw.fillText("Ball: " + ballScore, 500, 50, 200);

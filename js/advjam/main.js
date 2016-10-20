ee1dbf65d7e2cc6f22907f9a0707c1702b8a35b1
Ryan Roman <khluxordx@gmail.com>
2016-10-20 10:09:50 -0500

    Update main.js

--- js/advjam/main.js
+++ js/advjam/main.js
@@ -47,9 +47,9 @@ function initCanvas() {
       colin.src = "img/advjam/ColinRedesign.png";
       alex.src = "img/advjam/Alex.png";
       akira.src = "img/advjam/Akira.png";
-      /*foleyBase.src = "img/advjam/Foley Main Base.png";
+      foleyBase.src = "img/advjam/Foley Main Base.png";
       inventory.src = "img/advjam/Item Inventory.png";
-      map.src = "img/advjam/Map.png";
+      /*map.src = "img/advjam/Map.png";
       speech.src = "img/advjam/SpeechBubble1.png";
       speech1.src = "img/advjam/SpeechBubble2.png";
       mapMarker.src = "img/advjam/mapMarker.png";
